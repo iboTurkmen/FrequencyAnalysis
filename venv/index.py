de6ba549d3779315dea57e661da93e4665760700
iboturkmen <iboTurkmen92@outlook.com>
2020-04-23 09:29:39 +0300

    version 1.00

--- venv/index.py
+++ venv/index.py
@@ -1,15 +1,11 @@
 
 def DecryEngChar():
     result = ""
-    EngChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-               'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-               'U', 'V', 'W', 'X', 'Y', 'Z']
-    freq_letters = ['E', 'T', 'A', 'O', 'I', 'N', 'S', 'H', 'R',
-                    'D', 'L', 'C', 'U', 'M', 'W', 'F', 'G', 'Y',
-                    'P', 'B', 'V', 'K', 'J', 'X', 'Q', 'Z']
+    freq_letters = [' ', 'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r',
+                    'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y',
+                    'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z']
     freq_analysis = {}
     ciphertxt = input("Please Enter The Text To Decrypt: ").upper()
-    ciphertxt = ciphertxt.replace(' ', '')
     ciphertxt = ciphertxt.replace("'", '')
     ciphertxt = ciphertxt.replace('.', '')
     ciphertxt = ciphertxt.replace(',', '')
@@ -21,19 +17,72 @@ def DecryEngChar():
     freq_Numbers = sorted(freq_analysis.values())
     freq_Numbers.reverse()
 
-    maxfreq = max(freq_analysis, key=freq_analysis.get)
-
-    print(maxfreq)
+    for each in freq_letters:
+        maxfreq = max(freq_analysis, key=freq_analysis.get)
+        if each == " ":
+            result = ciphertxt.replace(maxfreq, each)
+            del freq_analysis[maxfreq]
+        if each == "e":
+            result = result.replace(maxfreq, each)
+            del freq_analysis[maxfreq]
+        if each == "t":
+            result = result.replace(maxfreq, each)
+            del freq_analysis[maxfreq]
+        if each == "i":
+            result = result.replace("I", "h")
+        if each == "g":
+            result = result.replace("G", "o")
+        if each == "f":
+            result = result.replace("F", "n")
+        if each == "s":
+            result = result.replace("L", "s")
+        if each == "d":
+            result = result.replace("D", "l")
+        if each == "v":
+            result = result.replace("V", "v")
+        if each == "k":
+            result = result.replace("K", "r")
+        if each == "q":
+            result = result.replace("Q", "u")
+        if each == "r":
+            result = result.replace("R", "g")
+        if each == "n":
+            result = result.replace("N", "c")
+        if each == "e":
+            result = result.replace("E", "m")
+        if each == "m":
+            result = result.replace("M", "a")
+        if each == "w":
+            result = result.replace("W", "w")
+        if each == "x":
+            result = result.replace("X", "x")
+        if each == "y":
+            result = result.replace("Y", "y")
+        if each == "z":
+            result = result.replace("Z", "z")
+        if each == "c":
+            result = result.replace("C", "f")
+        if each == "b":
+            result = result.replace("B", "k")
+        if each == "u":
+            result = result.replace("U", "d")
+        if each == "j":
+            result = result.replace("J", "q")
+        if each == "h":
+            result = result.replace("H", "p")
+        if each == "p":
+            result = result.replace("P", "i")
     print("Frequencies Of Letters in English Language:- " + str(freq_letters))
     print("Frequencies Of Ciphertext:- " + str(freq_Numbers))
     print("Frequencies Analysis Of Ciphertext:- " + str(freq_analysis))
+    print(result)
+    main()
 
 def DecryTurkChar():
     result = ""
     TurkChar = ['A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'Ğ', 'H',
                 'I', 'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö', 'P',
                 'R', 'S', 'Ş', 'T', 'U', 'Ü', 'V', 'Y', 'Z']
-
 
 
 def decryption():
